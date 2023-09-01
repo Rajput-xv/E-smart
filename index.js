54fb0da49420da79149e7d3119dee7c1b55c0110
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2023-09-01 23:14:25 +0530

    login html & css added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-document.querySelector(".login button").addEventListener("click",function(){
-    document.querySelector("a").href="login.html";
-})
+// document.querySelector(".login button").addEventListener("click",function(){
+//     document.querySelector("a").href="login.html";
+// })
+
+// document.querySelectorAll(".text-end button")[i].addEventListener("click",function(){
+//     location.href="http://127.0.0.1:3000/project/E-smart/login.html";
+//     window.location="http://127.0.0.1:3000/project/E-smart/login.html";
+
+// });
