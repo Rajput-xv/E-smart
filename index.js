a25f9aaf3af1929f4d1a5a3f870c3fdd67de45eb
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2023-09-07 19:50:30 +0530

    Testimonial section added HTML,CSS,JS

--- index.js
+++ index.js
@@ -7,3 +7,41 @@
 //     window.location="http://127.0.0.1:3000/project/E-smart/login.html";
 
 // });
+
+
+var btnTestimonial =document.querySelectorAll(".indicator .btn");
+var slide =document.getElementById("slide");
+
+
+btnTestimonial[0].onclick=function(){
+    slide.style.transform =" translateX(0px)";
+    for(i=0;i<4;i++){
+        btnTestimonial[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+btnTestimonial[1].onclick=function(){
+    slide.style.transform =" translateX(-800px)";
+    for(i=0;i<4;i++){
+        btnTestimonial[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+btnTestimonial[2].onclick=function(){
+    slide.style.transform =" translateX(-1600px)";
+    for(i=0;i<4;i++){
+        btnTestimonial[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+btnTestimonial[3].onclick=function(){
+    slide.style.transform =" translateX(-2400px)";
+    for(i=0;i<4;i++){
+        btnTestimonial[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
