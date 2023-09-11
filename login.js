0d7a35d8c0df8d53e0e77f2e467a478459d5cb61
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2023-09-11 22:43:13 +0530

    login html,css completed

---
+++ login.js
@@ -0,0 +1,13 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+
+registerLink.addEventListener('click', function(){
+    wrapper.classList.add('active');
+})
+
+
+loginLink.addEventListener('click', function(){
+    wrapper.classList.remove('active');
+})
